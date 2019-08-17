41e2d24366ca86614006407fa28d343cccb7436c
aelmosalamy <adham@elmosalamy.net>
2019-08-17 17:01:08 +0200

    Initial commit

---
+++ dice.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+<title>Dice Roller</title>
+</head>
+
+<body>
+<h1>--- Dice Roller ---</h1>
+<hr>
+<img src="dice/dice<?php echo rand(1, 6) ?>.png" width="200px">
+<p><button onclick="location.reload()">Roll</button></p>
+</body>
+
+</html
+
